13a01384326c557fd1ae316b415a2962c44eae7e
Amaury <amaury.pichon@hotmail.fr>
2018-11-20 19:32:36 +0100

    Ajout d'un DatePickerDialog afin de sélectionner la date.

--- app/src/main/java/fr/wildcodeschool/blablawild/ItinerarySearchActivity.java
+++ app/src/main/java/fr/wildcodeschool/blablawild/ItinerarySearchActivity.java
@@ -1,13 +1,17 @@
 package fr.wildcodeschool.blablawild;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.util.*;
+import java.text.*;
+
 
 public class ItinerarySearchActivity extends AppCompatActivity {
 
@@ -16,17 +20,48 @@ public class ItinerarySearchActivity extends AppCompatActivity {
     public static final String EXTRA_DATE = "fr.wildcodeschool.blablawild.DATE";
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itinerary_search);
+
+        final EditText editDate = (EditText) findViewById(R.id.editTextDate);
+        final Calendar myCalendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel(myCalendar, editDate);
+            }
+
+        };
+
+
+        editDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                new DatePickerDialog(ItinerarySearchActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
     }
 
     public void sendItinerary(View view){
 
         final EditText departure = findViewById(R.id.editTextDeparture);
         final EditText destination = findViewById(R.id.editTextDestination);
-        final EditText date = findViewById(R.id.editTextDate);
+        final EditText editDate = (EditText) findViewById(R.id.editTextDate);
+
 
         if (departure.getText().toString().matches("")||destination.getText().toString().matches("")){
             Toast searchToast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.search_error), Toast.LENGTH_SHORT);
@@ -37,8 +72,16 @@ public class ItinerarySearchActivity extends AppCompatActivity {
             Intent itineraryListIntent = new Intent(this, ItineraryListActivity.class);
             itineraryListIntent.putExtra(EXTRA_DEPARTURE, departure.getText().toString());
             itineraryListIntent.putExtra(EXTRA_DESTINATION, destination.getText().toString());
-            itineraryListIntent.putExtra(EXTRA_DATE, date.getText().toString());
+            itineraryListIntent.putExtra(EXTRA_DATE, editDate.getText().toString());
             startActivity(itineraryListIntent);
         }
     }
+
+    private void updateLabel(Calendar myCalendar, EditText editDate) {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        editDate.setText(sdf.format(myCalendar.getTime()));
+    }
+
 }
